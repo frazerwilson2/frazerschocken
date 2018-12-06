4c2c5c4871da60d4f9de5ce7fe959542494a039a
frazer wilson <frazerwilson145@gmail.com>
2018-12-06 19:18:25 +0000

    links and colour picker

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import './styles.scss';
-// import './fonticon.css';
-console.log('hello world 2');
+
+const colors = ['#63560d', '#963511', '#821730', '#291256', '#123554', '#223a29', '#4e1358'];
+let currentColour = null;
+
+const colourChange = ()=>{
+ const len = colors.length;
+ const pick = Math.floor(Math.random() * len);
+ if(colors[pick] == currentColour){
+ 	colourChange();
+ }
+ else {
+	document.documentElement.style.setProperty('--bg', colors[pick]);
+	currentColour = colors[pick];
+ }
+}
+
+colourChange();
+
+document.querySelector('#colourPick').addEventListener('click', colourChange);
